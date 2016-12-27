d883899889a2b003cc8369b8447318555592f520
SeongSoo Cho <nexusz99@gmail.com>
2016-12-27 17:29:48 +0900

    spring 설정 및 테스트 페이지 출력 구현 완료

---
+++ src/main/java/kr/printf/visitorbook/controllers/index.java
@@ -0,0 +1,23 @@
+package kr.printf.visitorbook.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by nexusz99 on 27/12/2016.
+ */
+@Controller
+public class index {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "hello world";
+    }
+}
